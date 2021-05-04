8dc2fcf3f476e50d11bac7f04cd70b58377cc40b
study-hary-id <haryansyah.study.id@gmail.com>
2021-05-04 14:58:04 +0700

    Create model Add Question and Choice models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.TextField()
+
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
