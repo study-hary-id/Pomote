6ceb4c5eaa90cf59d5cb277e25fdc836ea1ac448
study-hary-id <haryansyah.study.id@gmail.com>
2021-05-05 14:31:37 +0700

    Create some views Add home, create, vote, result views

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import ChoiceForm, QuestionForm
+
+def home(request):
+    return render(request, 'polls/home.html', dict())
+
+
+def create(request):
+    question_form = QuestionForm()
+    choice_form = ChoiceForm()
+
+    return render(request, 'polls/create.html', {
+        'question_form': question_form,
+        'choice_form': choice_form,
+    })
+
+
+def vote(request, poll_id):
+    return render(request, 'polls/vote.html', dict())
+
+
+def result(request, poll_id):
+    return render(request, 'polls/result.html', dict())
